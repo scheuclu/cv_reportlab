5913c61cc6c467c40697db37f48147baa757d2d1
Lukas Scheucher <scheuclu@gmail.com>
2024-02-08 15:15:29 +0100

    Feb 2024

--- main.py
+++ main.py
@@ -1,130 +1,177 @@
 from reportlab.pdfgen import canvas
 import yaml
-
-from reportlab.lib import colors
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.pagesizes import letter
+from reportlab.platypus.flowables import HRFlowable, KeepTogether
 
-FONTSIZE_SECTION=12
-FONTGAP_SECTION=1.2
 
-FONTSIZE_BODY=10
-FONTGAP_BODY=1.2
+from utils import bullet_points
 
-MARGIN_LEFT=20
-MARGIN_RIGHT=20
-MARGIN_TOP=40
 
-# Define a style for the bulleted list
-styles = getSampleStyleSheet()
-bullet_style = ParagraphStyle(
-    "BulletStyle",
-    parent=styles["BodyText"],
-    spaceBefore=6,
-    bulletIndent=20,
-    leftIndent=10,
-    bulletFontName="Helvetica",
-    bulletFontSize=10,
+document = SimpleDocTemplate(
+    "cv_lukas_scheucher.pdf",
+    pagesize=letter,
+    leftMargin=50,
+    rightMargin=50,
+    topMargin=30,
+    bottomMargin=50,
 )
+import re
+
+FONTSIZE_SECTION = 12
+FONTGAP_SECTION = 1.2
+
+FONTSIZE_BODY = 10
+FONTGAP_BODY = 1.2
+
+MARGIN_LEFT = 20
+MARGIN_RIGHT = 20
+MARGIN_TOP = 40
 
 
-def print_points(pdf, points,  xmin, ystart, font="Helvetica", font_size=10):
-    pdf.setFont(font, font_size)
-    y=ystart
-    for item in points:
-        paragraph = Paragraph(f"• {item}", bullet_style)
-        paragraph.wrapOn(pdf, width - MARGIN_RIGHT, xmin)
-        print(paragraph.blPara.lines)
-        try:
-            for x, text in paragraph.blPara.lines:
-                pdf.drawString(xmin, y, ' '.join(text))
-                y -= font_size
-        except:
-            pass
-    return y
+styles = getSampleStyleSheet()
+style_section = styles["Heading3"].clone("section")
+style_section.fontName = "Helvetica-Bold"
+style_section.fontSize = 14
+style_section.spaceBefore = 16
 
+
+style_subsection = styles["Heading4"].clone("section")
+style_subsection.leading = 0
+style_subsection.spaceAfter = 0
+style_subsection.spaceBefore = 12
+style_subsection.fontName = "Helvetica-bold"
+
+
+style_date = styles["BodyText"].clone("date")
+style_date.alignment = 2  # 0=left, 1=center, 2=right
+style_date.spaceBefore = 0
+style_date.spaceAfter = 4
+
+
+style_section_points = styles["Normal"].clone("style_section_points")
+style_subsection_points = styles["Normal"].clone("style_section_points")
+
+style_cv_title = styles["Heading1"].clone("style_cv_title")
+style_cv_title.fontSize = 28
+style_cv_title.spaceAfter = 14
+# style_cv_title.textColor='#092f61'
+
+
+style_socials = styles["Normal"].clone("style_socials")
+# style_socials.textColor='#092f61'
+
+
+def header(name, mail=None, phone=None, github=None, linkedin=None):
+    lines = []
+    title = Paragraph(name, style_cv_title)
+    lines.append(title)
+    symbols = []
+    if mail:
+        symbols.append(
+            f'<a href="mailto:{mail}"><img src="mail-icon.png" height="4mm" width="4mm"/>   <b>{mail}</b></a>'
+        )
+    if phone:
+        symbols.append(
+            f'<a href="tel: {phone}"><img src="phone-icon.png" height="4mm" width="4mm"/>   <b>{phone}</b></a>'
+        )
+    if github:
+        symbols.append(
+            f'<a href="https://www.github.com/{github}"><img src="github-icon.png" height="4mm" width="4mm"/>   <b>{github}</b></a>'
+        )
+    if linkedin:
+        symbols.append(
+            f'<a href="https://www.linkedin.com/in/{linkedin}"><img src="linkedin-icon.png" height="4mm" width="4mm"/>   <b>{linkedin}</b></a>'
+        )
+
+    lines.append(Paragraph("&nbsp;&nbsp;|&nbsp;&nbsp;".join(symbols), style_socials))
+    lines.append(
+        HRFlowable(
+            width="100%",
+            thickness=2,
+            lineCap="round",
+            color="black",
+            spaceBefore=12,
+            spaceAfter=1,
+        )
+    )
+    return lines
 
 
 if __name__ == "__main__":
     # Specify the file path for the PDF
     pdf_file = "example.pdf"
 
-
-
-    infile='cv.yaml'
+    infile = "cv.yaml"
     with open(infile, "r") as f:
-        t=f.read()
-        #t=markdown_links(t)
+        t = f.read()
+        # t=markdown_links(t)
         try:
             cv = yaml.safe_load(t)
             ###print(result)
         except yaml.YAMLError as exc:
             print(exc)
 
-
     pdf = canvas.Canvas(pdf_file, pagesize=letter)
-    pdf.setFont("Helvetica", 12)
+    pdf.setFont("Courier-Oblique", 12)
     # Set the starting point for writing
-
 
     width, height = letter
     center_x = width / 2
-    y = height-MARGIN_TOP
+    y = height - MARGIN_TOP
 
-    pdf.setFont("Helvetica-Bold", 32)
-    headline_text="Lukas <b>bold</b> Scheucher"
-    x_coordinate = (width - pdf.stringWidth(headline_text, "Helvetica-Bold", 16)) / 2
-    pdf.drawCentredString(x_coordinate, y, headline_text)
-    y-=32
-    pdf.setFont("Helvetica", 12)
-    pdf.drawString(MARGIN_LEFT, y, "lukas@scheuclu.com | (+43)-677-6100-3595 | scheuclu | scheuclu")
-    y -= 28
+    doc = []
+    title = Paragraph("Lukas Scheucher", style_cv_title)
 
-    pdf.setFont("Helvetica", 12)
+    doc += header(
+        name="Lukas Scheucher",
+        mail="lukas@scheuclu.com",
+        phone="0043-677-6100-3595",
+        github="scheuclu",
+        linkedin="scheuclu",
+    )
 
-    for section in cv['sections']:
-        heading=section['heading']
-        print(heading)
-        subsections=section['subsections'] if 'subsections' in section else []
-        points=section['points'] if 'points' in section else []
-        print(points)
-
-
-        text_width = pdf.stringWidth(heading, "Helvetica", 12)
-        x = center_x - (text_width / 2)
-        pdf.line(MARGIN_LEFT, y+6, x-FONTSIZE_SECTION, y+6)
-        pdf.drawString(x, y, heading)
-        pdf.line(x+text_width+FONTSIZE_SECTION, y + 6, width-MARGIN_RIGHT, y + 6)
-        y -= int(FONTSIZE_SECTION*FONTGAP_SECTION*2)
-        y = print_points(pdf, points, MARGIN_LEFT, ystart=y, font="Helvetica", font_size=10)
+    for section in cv["sections"]:
+        heading = section["heading"]
+        doc.append(Paragraph(f"<u>{heading}</u>", style_section))
+        subsections = section["subsections"] if "subsections" in section else []
+        section_points = section["points"] if "points" in section else []
 
         for subsection in subsections:
-            subsection_heading=subsection['heading']['label']
-            subsection_points = subsection['points'] if 'points' in subsection else []
-            print(subsection.keys())
-            pdf.drawString(MARGIN_LEFT, y, subsection_heading)
-            y -= FONTSIZE_SECTION * FONTGAP_SECTION
+            subsection_heading = subsection["heading"]["label"]
+            startdate = (
+                subsection["heading"]["startdate"]
+                if "startdate" in subsection["heading"]
+                else None
+            )
+            enddate = (
+                subsection["heading"]["enddate"]
+                if "enddate" in subsection["heading"]
+                else None
+            )
+            subsection_points = subsection["points"] if "points" in subsection else []
+            subsection_degrees = (
+                subsection["degrees"] if "degrees" in subsection else []
+            )
+            doc.append(Paragraph(subsection_heading, style_subsection))
+            if startdate and enddate:
+                doc.append(Paragraph(f"{startdate} - {enddate}", style_date))
+            else:
+                doc.append(Spacer(1, 12))
+            doc += bullet_points(subsection_points, style=style_subsection_points)
+            doc += bullet_points(subsection_degrees, style=style_subsection_points)
+        doc += bullet_points(section_points, style=style_section_points)
+        doc.append(
+            HRFlowable(
+                width="100%",
+                thickness=1,
+                lineCap="round",
+                color="#bbbbbb",
+                spaceBefore=12,
+                spaceAfter=0,
+            )
+        )
 
-            # pdf.setFont("Helvetica", 10)
-            # for item in subsection_points:
-            #     paragraph = Paragraph(f"• {item}", bullet_style)
-            #     paragraph.wrapOn(pdf, width-MARGIN_RIGHT, MARGIN_LEFT)
-            #     print(paragraph.blPara.lines)
-            #     for x, text in paragraph.blPara.lines:
-            #         pdf.drawString(MARGIN_LEFT, y, ' '.join(text))
-            #         y -= 10
-            y=print_points(pdf, subsection_points, xmin=MARGIN_LEFT, ystart=y, font="Helvetica", font_size=10)
-            pdf.setFont("Helvetica", 12)
-                # y -= max(paragraph.height*(len(paragraph.blPara.lines)-1),0)# Adjust width as needed
-                # paragraph.drawOn(pdf, MARGIN_LEFT, y)
-                # y -= paragraph.height*len(paragraph.blPara.lines)
-                #y -= FONTSIZE_SECTION * FONTGAP_SECTION
-                #break
-
-        #break
-
-    pdf.save()
-
+    document.build(doc)
     print(f"PDF created successfully: {pdf_file}")
---
+++ utils.py
@@ -0,0 +1,64 @@
+import re
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+
+styles = getSampleStyleSheet()
+
+
+def bullet_points(points, style=styles["Normal"]):
+    bullets = []
+    for point in points:
+        if isinstance(point, str):
+            point = markdown_replace(point)
+            bullets.append(Paragraph("<bullet>&bull;</bullet> " + point, style))
+        if isinstance(point, list):
+            for subpoint in point:
+                subpoint = markdown_replace(subpoint)
+                bullets.append(
+                    Paragraph(
+                        "<bullet>&nbsp;&nbsp;&nbsp;&nbsp;&bull;</bullet> " + subpoint,
+                        style,
+                    )
+                )
+    return bullets
+
+
+def _bold(s):
+    # s=' '+s
+    sp = s.split("**")
+    if len(sp) == 1:
+        return s
+    r = [" <b>", "</b>"]
+    i = 0
+    result = ""
+    result += sp[0]
+    for spp in sp[1:]:
+        result += r[i]
+        i = (i + 1) % 2
+        result += spp
+
+    # print("--",result)
+    return result
+
+
+def _markdown_links(s):
+    print(s)
+    site = re.compile("\[(.*)\]").search(s)
+    url = re.compile("\((.*)\)").search(s)
+
+    # "aaaa [Full list on LinkedIn](https://www.linkedin.com/in/scheuclu)"
+    # "aaa  <a href='TODO'> word </a>
+
+    if site is not None and url is not None:
+        print(site.group(1), url.group(1))
+        # return s
+        s = s.replace(
+            f"[{site.group(1)}]({url.group(1)})",
+            f"<a href='{url.group(1)})'> {site.group(1)} </a>",
+        )
+
+    return s
+
+
+def markdown_replace(s):
+    return _markdown_links(_bold(s))
